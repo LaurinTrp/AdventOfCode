8e40e89c1e315a4182f7e6789e8ad5b9affc046c
laurin <laurin@laurin-acer>
2021-12-02 11:48:20 +0100

    Day 2 2021

---
+++ 2021/Day2/Day2_2021.java
@@ -0,0 +1,34 @@
+package Day2;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import Globals.FileReader;
+
+public class Day2_2021 {
+	public static void main(String[] args) {
+		ArrayList<String> lines = FileReader.getFileContent(Paths.get("").toAbsolutePath().toString(),
+				"/2021/Day2/input.txt");
+		int horizontal = 0, vertical = 0, aim = 0, depth = 0;
+		for (String line : lines) {
+			String[] split = line.split(" ");
+			int value = Integer.parseInt(split[1]);
+			switch (split[0]) {
+				case "forward":
+					horizontal += value;
+					depth += aim*value;
+					break;
+				case "down":
+					vertical+=value;
+					aim += value;
+					break;
+				case "up":
+					vertical-=value;
+					aim -= value;
+					break;
+			}
+		}
+		System.out.println(horizontal * vertical);
+		System.out.println(horizontal * depth);
+	}
+}
