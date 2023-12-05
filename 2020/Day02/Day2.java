aa4ab3f92601b8cf5ecbc55cc87a3bf59b252967
Laurin Trapp <18750@nordakademie.de>
2023-12-05 19:11:58 +0100

    Did some stuff from 2020

--- 2020/Day02/Day2.java
+++ 2020/Day02/Day2.java
@@ -5,41 +5,45 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import Loaders.ResourceLoader;
 
 public class Day2 {
 	public static void main(String[] args) {
 
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new FileReader(
-					new File("E:\\Programmieren\\Java\\W�rkbensch\\AdventOfCode\\2020\\Day2\\PasswordList")));
-			ArrayList<String[]> strings = new ArrayList<>();
-			String line = reader.readLine();
-			while (line != null) {
-				strings.add(line.split(":"));
-				line = reader.readLine();
-			}
-			int lower, upper, valids = 0;
-			char searched;
-			for (String[] string : strings) {
-				for (int i = 0; i < string.length; i++) {
-					string[i] = string[i].trim();
-				}
-			}
-			for (String[] string : strings) {
-				lower = Integer.parseInt(string[0].split("-")[0]);
-				upper = Integer.parseInt(string[0].split("-")[1].split(" ")[0]);
-				searched = string[0].charAt(string[0].length() - 1);
+		List<String> lines = ResourceLoader.getContentAsLines("2020", "Day_02" + File.separator + "Test.txt");
 
-				if (string[1].charAt(lower-1) != string[1].charAt(upper - 1)) {
-					if (string[1].charAt(lower - 1) == searched || string[1].charAt(upper - 1) == searched) {
-						valids++;
-						System.out.println(lower + "\t" + upper + "\t" + searched + "\t" + string[1] + "\t" + valids);
-					}
-				}
+		partOne(lines);
+	}
+
+	private static void partOne(List<String> lines) {
+		List<String[]> splittedList = lines.stream().map(x -> {
+			return x.split(":");
+		}).collect(Collectors.toList());
+		
+		int lower, upper, valids = 0;
+		String searched[] = new String[1];
+		Pattern patternNumber = Pattern.compile("\\d{1,}");
+		for (String[] splitted : splittedList) {
+			Matcher matcher = patternNumber.matcher(splitted[0]);
+			matcher.find();
+			
+			lower = Integer.parseInt(matcher.group());
+			upper = Integer.parseInt(matcher.group());
+			searched[0] = splitted[0].substring(splitted[0].length() - 1);
+
+			long count = splitted[1].chars().filter(x -> {
+				return x == searched[0].charAt(0);
+			}).count();
+			
+			if(count >= lower && count <= upper) {
+				valids++;
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
+		System.out.println(valids);
 	}
 }
